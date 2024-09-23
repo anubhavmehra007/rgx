92a1c3174a591ee293c6c536b326411328b9b9e6
Anubhav Mehra <anubhav.mehra@hotmail.com>
2024-09-23 22:31:14 +0530

    Added parseRepeate and parseGroup and parseBracket

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-//Types
 
 type tokenType uint8
 
@@ -21,14 +20,6 @@ type parseContext struct {
 	tokens []token
 }
 
-//Types End
-
-
-/*
-Function parse
-Input string
-Ouput *parseContext
-/*
 
 func parse(regex string) *parseContext {
 	ctx := & parseContext{
@@ -42,12 +33,6 @@ func parse(regex string) *parseContext {
 	return ctx
 }
 
-
-/*
-Function process
-Input string, *parseContext
-Ouput void
-/*
 
 func process(regex string, ctx *parseContext) {
 	ch := regex[ctx.pos]
@@ -78,5 +63,97 @@ func process(regex string, ctx *parseContext) {
 		ctx.tokens = append(ctx.tokens, t)
 	}
 }
+
+
+func parseGroup(regex string, ctx *parseContext) {
+	ctx.pos +=1
+	for regex[ctx.pos] != ')' {
+		process(regex, ctx)
+		ctx.pos+=1
+}
+}
+
+func parseBracket(regex string, ctx *parseContext) {
+	ctx.pos++
+	var literals []string
+	for regex[ctx.pos] != ']' {
+		ch := regex[ctx.pos]
+		if ch == '-' {
+			next := regex[ctx.pos+1]
+			prev := literals[len(literals)-1][0]
+			literals[len(literals) - 1] = fmt.Sprintf("%c%c", prev, next)
+			ctx.pos++
+		} else {
+			literals = append(literals, fmt.Sprintf("%c", ch))
+		}
+		ctx.pos++
+	}
+	literalsSet := map[uint8]bool{}
+	for _, l : range literals {
+		for i := l[0]; i <= l[len(l)-1]; i++ {
+			literalSet[i] = true
+
+		}
+	}
+	ctx.tokens = append(ctx.tokens, token{
+		tokenType: bracket,
+		value: literalSet,
+	})
+}
+
+func parseOr(regex string, ctx *parseContext) {
+	rhsContext := &parseContext {
+		pos: ctx.pos,
+		tokens: []token{},
+	}
+	rhsContext.pos +=1
+	for rhsContext.pos < len(regex) && regex[rhsContext.pos] != ')' {
+		process(regex, rhsContext)
+		rhsContext.pos +=1
+	}
+
+	left := token {
+		tokenType: groupUncaptured,
+		value: ctx.tokens,
+	}
+
+	right: token{
+		tokenType: groupUncaptured,
+		value: rhsContext.tokens
+	}
+	ctx.pos = rhsContext.pos
+	ctx.tokens = []token{{
+		tokenType: or,
+		value: []token{left, right},
+	}}
+}
+
+const repeatInfinity = -1
+
+func parseRepeate(regex string, ctx *parseContext) {
+	ch := regex[ctx.pos]
+	var min, max int
+	switch ch {
+	case '*':
+		min = 0
+		max = repeatInfinity
+	case '?':
+		min = 0
+		max = 1
+	default:
+		min = 1
+		max = repeatInfinity
+	}
+	lastToken = ctx.tokens[len(ctx.tokens) - 1]
+	ctx.tokens[len(ctx.tokens) - 1] = token{
+		tokenType: repeat,
+		value: repeatPayload{
+			min: min,
+			max: max,
+			token: lastToken
+		}
+	}
+}
+
 
 
